04eac4f09084478f1832012dd04de4730fcfefdc
Pravin Agravat <pravin@nium.com>
2021-08-20 10:15:19 +0530

    Initial Commit

---
+++ HirenPrectical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HirenPrectical
+//
+//  Created by IOSDEV1 on 20/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
